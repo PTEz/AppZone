84b717ba776a868b536de465b338921c99f46093
Patrick Boos <boos_patrick@cyberagent.co.jp>
2013-01-16 12:30:46 +0900

    Jenkins: Do not create manifest since that one gets generated on server

--- jenkins-plugin/src/main/java/jp/cyberagent/jenkins/plugins/DeployStrategyIOs.java
+++ jenkins-plugin/src/main/java/jp/cyberagent/jenkins/plugins/DeployStrategyIOs.java
@@ -6,8 +6,6 @@ import hudson.model.AbstractBuild;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.StringWriter;
 import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -16,12 +14,10 @@ import java.util.regex.Pattern;
 import org.apache.commons.httpclient.methods.multipart.FilePart;
 import org.apache.commons.httpclient.methods.multipart.Part;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.AbstractFileFilter;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 
-import com.dd.plist.NSArray;
 import com.dd.plist.NSDictionary;
 import com.dd.plist.PropertyListParser;
 
@@ -36,7 +32,6 @@ class DeployStrategyIOs extends DeployStrategy {
         super(server, "ios", id, tag, prependNameToTag, build, listener);
         mIpaFile = ipaFile;
         mPlistFile = findPlistFile();
-        createManifest();
     }
 
     private File findPlistFile() {
@@ -72,65 +67,30 @@ class DeployStrategyIOs extends DeployStrategy {
     public List<Part> getParts() throws FileNotFoundException {
         List<Part> parts = super.getParts();
         parts.add(new FilePart("ipa", mIpaFile));
-        parts.add(new FilePart("manifest", getManifestFile()));
         return parts;
     }
 
     @Override
     public String getVersion() {
         try {
-            File file = getManifestFile();
-            NSDictionary rootDict = (NSDictionary) PropertyListParser.parse(file);
-            NSArray items = (NSArray) rootDict.objectForKey("items");
-            NSDictionary item = (NSDictionary) items.objectAtIndex(0);
-            NSDictionary metadata = (NSDictionary) item.objectForKey("metadata");
-            return metadata.objectForKey("bundle-version").toString();
+            return getBundleVersionVersion();
         } catch (Exception e) {
             getLogger().println(TAG + "Error: " + e.getMessage());
         }
         return super.getVersion();
     }
 
-    private boolean createManifest() {
-        File manifestFile = getManifestFile();
-        getLogger().println(TAG + "Creating manifest: " + manifestFile.getName());
-        try {
-            if (mPlistFile == null) {
-                getLogger().println(TAG + "No Info.plist file found. Aborting.");
-                return false;
-            }
-            NSDictionary rootDict = (NSDictionary) PropertyListParser.parse(mPlistFile);
-            String bundleName = rootDict.objectForKey("CFBundleName")
-                    .toString();
-            String bundleIdentifier = rootDict.objectForKey("CFBundleIdentifier")
-                    .toString();
-            String bundleShortVersionString = rootDict.objectForKey("CFBundleShortVersionString")
-                    .toString();
-            String bundleVersion = rootDict.objectForKey("CFBundleVersion").toString();
-
-            InputStream inputStream = getClass().getResourceAsStream(
-                    "/OTAManifestTemplate.plist");
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(inputStream, writer, "UTF-8");
-            String manifest = writer.toString();
-
-            manifest = manifest.replace("${url}", getUrl() + ".ipa");
-            manifest = manifest.replace("${CFBundleName}", bundleName);
-            manifest = manifest.replace("${CFBundleIdentifier}", bundleIdentifier);
-            manifest = manifest.replace("${CFBundleShortVersionString}",
-                    bundleShortVersionString + " (" + bundleVersion + ")");
-            FileUtils.writeStringToFile(manifestFile, manifest);
-        } catch (Exception e) {
-            getLogger().println(
-                    TAG + "Problmen creating manifest based on Info.plist: " + e.getMessage());
-            return false;
+    private String getBundleVersionVersion() throws Exception {
+        if (mPlistFile == null) {
+            getLogger().println(TAG + "No Info.plist file found. Aborting.");
+            return null;
         }
-        return true;
-    }
+        NSDictionary rootDict = (NSDictionary) PropertyListParser.parse(mPlistFile);
 
-    private File getManifestFile() {
-        String path = mIpaFile.getAbsolutePath();
-        return new File(path.substring(0, path.length() - 4) + ".manifest");
+        String bundleShortVersionString = rootDict.objectForKey("CFBundleShortVersionString")
+                .toString();
+        String bundleVersion = rootDict.objectForKey("CFBundleVersion").toString();
+        return bundleShortVersionString + " (" + bundleVersion + ")";
     }
 
     @Override
