196f01de763f9fd0b32bc7b4627cd89cc3f84e3d
Patrick Boos <boos_patrick@cyberagent.co.jp>
2013-03-28 14:50:00 +0900

    Add ScalatraListener so the API works correctly

--- server/src/main/scala/jp/cyberagent/appzone/JettyLauncher.scala
+++ server/src/main/scala/jp/cyberagent/appzone/JettyLauncher.scala
@@ -2,6 +2,7 @@ package jp.cyberagent.appzone
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
 import jp.cyberagent.appzone.AppZoneServlet
 
 object JettyLauncher { // this is my entry object as specified in sbt project definition
@@ -12,8 +13,12 @@ object JettyLauncher { // this is my entry object as specified in sbt project de
     val context = new WebAppContext()
     context setContextPath "/"
     context.setResourceBase("src/main/webapp")
+
+    context.addEventListener(new org.scalatra.servlet.ScalatraListener())
+
     context.addServlet(classOf[AppZoneServlet], "/*")
-    context.addServlet(classOf[DefaultServlet], "/")
+    // context.addServlet(classOf[DefaultServlet], "/")
+
 
     server.setHandler(context)
 
