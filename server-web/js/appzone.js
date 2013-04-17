471f0db7a58d391f4877c968f4f681b87da2d8c7
Patrick Boos <boos_patrick@cyberagent.co.jp>
2013-04-17 11:36:01 +0900

    Fix: Wrong api server name

--- server-web/js/appzone.js
+++ server-web/js/appzone.js
@@ -1,6 +1,6 @@
 /*global Backbone,_ */
 (function() {
-var SERVER = 'http://appzone-api.idcf.nbu.ca.local/';
+var SERVER = 'http://appzone-api.idcf.prd.nbu.ca.local/';
 var AppItem = Backbone.Model.extend({
   url: function() { return SERVER + 'app/' + this.id; },
   clear: function() {
